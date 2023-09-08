eb7005a3b1126967ada94331fc9cc98910ff4da4
Stefan Bilharz <stefan@sbsoftware.de>
2023-09-08 16:08:43 +0200

    Add a spec for a module registering a stimulus controller

---
+++ spec/js_module/stimulus_spec.cr
@@ -0,0 +1,39 @@
+require "../spec_helper"
+
+module JsModule::StimulusSpec
+  class MyModule < JsModule
+    js_import Application, Controller, from: "/assets/stimulus.js"
+
+    js_class MyController do
+      js_extends Controller
+
+      js_method :connect do
+        console.log("connected!")
+      end
+    end
+
+    def_to_js do
+      window.Stimulus = Application.start
+
+      Stimulus.register("my", MyController)
+    end
+  end
+
+  describe "MyModule.to_js" do
+    it "should return the correct JS code" do
+      expected = <<-JS.squish
+      import { Application, Controller } from "/assets/stimulus.js";
+      class MyController extends Controller {
+        connect() {
+          console.log("connected!");
+        }
+      }
+
+      window.Stimulus = Application.start();
+      Stimulus.register("my", MyController);
+      JS
+
+      MyModule.to_js.should eq(expected)
+    end
+  end
+end
