11838678c1f2e66cd1df50cb4fbdd60bcaf718ff
Stefan Bilharz <stefan@sbsoftware.de>
2025-07-28 21:39:29 +0200

    JS-40 Refactor nested ifs

--- src/js/code.cr
+++ src/js/code.cr
@@ -157,24 +157,12 @@ module JS
           {% elsif exp.is_a?(Path) %}
             {% parent_namespace = namespace.stringify.split("::")[0..-2].join("::").id %}
             {% relative_path = exp.global? ? exp.stringify.gsub(/\A::/, "") : exp %}
-            {% if exp.resolve? %}
-              if {{exp}}.responds_to?(:to_js_ref)
-                {{io}} << {{exp}}.to_js_ref
-              else
-                {{io}} << {{exp.stringify}}
-              end
-            {% elsif parse_type("#{namespace}::#{relative_path.id}").resolve? %}
-              if {{namespace}}::{{exp}}.responds_to?(:to_js_ref)
-                {{io}} << {{namespace}}::{{exp}}.to_js_ref
-              else
-                {{io}} << {{exp.stringify}}
-              end
-            {% elsif parse_type("#{parent_namespace}::#{relative_path.id}").resolve? %}
-              if {{parent_namespace}}::{{exp}}.responds_to?(:to_js_ref)
-                {{io}} << {{parent_namespace}}::{{exp}}.to_js_ref
-              else
-                {{io}} << {{exp.stringify}}
-              end
+            {% if (type = exp.resolve?) && type.class.has_method?("to_js_ref") %}
+              {{io}} << {{exp}}.to_js_ref
+            {% elsif (type = parse_type("#{namespace}::#{relative_path.id}").resolve?) && type.class.has_method?("to_js_ref") %}
+              {{io}} << {{type}}.to_js_ref
+            {% elsif (type = parse_type("#{parent_namespace}::#{relative_path.id}").resolve?) && type.class.has_method?("to_js_ref") %}
+              {{io}} << {{type}}.to_js_ref
             {% else %}
               {{io}} << {{exp.stringify}}
             {% end %}
